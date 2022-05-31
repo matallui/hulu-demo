9f3d8835a2a88cf693a6a6b1519b7772a1559c9c
Luis Taniça <ltanica@mersive.com>
2022-05-31 16:46:38 -0600

    disable FlipMove because of build errors

--- components/Results.tsx
+++ components/Results.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Thumbnail from './Thumbnail';
-import FlipMove from 'react-flip-move';
+// import FlipMove from 'react-flip-move';
 
 interface Props {
   results: any[];
@@ -9,14 +9,18 @@ interface Props {
 const Results: React.FC<Props> = ({ results }) => {
   return (
     // https://github.com/joshwcomeau/react-flip-move/issues/269
-    <FlipMove
+    // <FlipMove
+    //   className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3
+    //   3xl:flex flex-wrap justify-center'
+    // >
+    <div
       className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3
       3xl:flex flex-wrap justify-center'
     >
       {results.map((result) => (
         <Thumbnail key={result.id} result={result} />
       ))}
-    </FlipMove>
+    </div>
   );
 };
 
